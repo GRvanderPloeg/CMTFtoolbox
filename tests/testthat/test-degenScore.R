ba81744a2e2aa17826dfe618168bbc3f278d882c
GRvanderPloeg <g.r.ploeg@uva.nl>
2025-04-27 11:58:18 +0200

    Implementation of ACMTFR_modelSelection

--- tests/testthat/test-degenScore.R
+++ tests/testthat/test-degenScore.R
@@ -1,4 +1,4 @@
 test_that("the function works normally", {
-  S <- matrix(rnorm(100), nrow = 10, ncol = 10)
-  degenScore(S)
+  S = matrix(rnorm(100), nrow = 10, ncol = 10)
+  expect_no_error(degenScore(S))
 })
